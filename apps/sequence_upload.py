00bdd770c2a591f5a1fb1b6fd4525ae10782d596
Kamil Pytlak <51530331+kamilpytlak@users.noreply.github.com>
2022-05-27 21:37:51 +0200

    Add sequence_upload.py

--- apps/sequence_upload.py
+++ apps/sequence_upload.py
@@ -1,5 +1,23 @@
+import io
+
 import streamlit as st
+from Bio import SeqIO
 
 
-def app():
-    pass
+def upload_file(file_or_path, is_uploaded: bool) -> (None, dict):
+    text_obj = file_or_path.read().decode("UTF-8") if is_uploaded else open(file_or_path, encoding="UTF-8").read()
+    dna_record = SeqIO.to_dict(SeqIO.parse(io.StringIO(text_obj), "fasta"))
+    return dna_record if dna_record else None
+
+
+def process_file(uploaded_file, dna_record):
+    if dna_record:
+        file_name = uploaded_file if isinstance(uploaded_file, str) else uploaded_file.name
+        st.session_state["file_cached"] = dna_record
+        st.success("File correctly uploaded. Validation successfully completed.")
+    else:
+        dna_record, file_name = None, None
+        st.error("Error while processing a file. Make sure you have uploaded a FASTA file.")
+    st.session_state["file_cached"] = dna_record
+    st.session_state["file_name"] = file_name
+    return dna_record, file_name
