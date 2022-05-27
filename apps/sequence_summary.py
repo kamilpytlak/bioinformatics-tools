6ca161f02ad148c13515a043776dc838dcd5e22a
Kamil Pytlak <51530331+kamilpytlak@users.noreply.github.com>
2022-05-27 21:38:11 +0200

    Add sequence_summary.py

--- apps/sequence_summary.py
+++ apps/sequence_summary.py
@@ -1,18 +1,66 @@
-import io
+from collections import Counter
 
-from Bio import SeqIO
-from Bio.Seq import Seq
+import pandas as pd
+from scipy.stats import chisquare
+import plotly.express as px
+import plotly.figure_factory as ff
 from Bio.SeqUtils import GC
 
 import streamlit as st
 
+SEQUENCE_SUMMARY_INFO = open("assets/info/sequence_summary_info.md").read()
+SEQUENCE_SUMMARY_ICON = "assets/images/summary_icon.png"
 
-def app():
-    seq_file = st.file_uploader("Upload a FASTA file", type=["fasta", "fa", "txt"])
+def check_seq_dist(seq_count: pd.Series):
+    df = len(seq_count) - 1
+    chi_stat, p_value = chisquare(seq_count)
+    chi_stat, p_value = chi_stat.round(2), p_value.round(2)
+    if p_value > 0.05:
+        st.write(rf"""
+        No basis for rejecting the hypothesis of sequence homogeneity
+        ($\chi^2_{df} \approx {chi_stat}; p \approx {p_value}$)
+        """)
+    else:
+        st.write(rf"""
+        The sequence is not homogeneous
+        ($\chi^2_{df} \approx {chi_stat}; p \approx {p_value}$)
+        """)
 
-    if st.button("OK") and seq_file:
-        byte_str = seq_file.read()
-        text_obj = byte_str.decode("UTF-8")
 
-        dna_record = SeqIO.read(io.StringIO(text_obj), "fasta")
-        st.write(dna_record.id)
+def summary(record):
+    seq = record.seq
+    seq_name = record.name
+    seq_len = len(seq)
+    seq_freq = pd.DataFrame.from_dict(Counter(seq), orient="index").reset_index()
+    seq_freq.columns = ["Nucleotide", "Count"]
+    seq_freq["Frequency"] = (seq_freq["Count"] / seq_freq["Count"].sum()).round(2)
+
+    st.write(f"**Record name:** {seq_name}")
+
+    st.write(f"**Overall record length:** {seq_len} nucleotides")
+
+    st.write(f"**Nucleotide distribution:**")
+    fig_table = ff.create_table(seq_freq)
+    fig_dist = px.bar(seq_freq, x="Nucleotide", y="Count")
+    st.plotly_chart(fig_table)
+    st.plotly_chart(fig_dist)
+    st.write(f"**GC content:** {round(GC(seq), 2)}%")
+    check_seq_dist(seq_freq["Count"])
+
+    st.markdown("---")
+
+
+def app(dna_record):
+    col_image, col_text = st.columns([1, 3])
+    with col_image:
+        st.image(SEQUENCE_SUMMARY_ICON)
+    with col_text:
+        st.write(SEQUENCE_SUMMARY_INFO)
+
+    if dna_record:
+        seq_names = st.multiselect("Select one or more sequences for analysis", dna_record.keys())
+        for seq_name in seq_names:
+            record = dna_record[seq_name]
+            summary(record)
+    else:
+        st.error("No cached data. Upload a file.")
