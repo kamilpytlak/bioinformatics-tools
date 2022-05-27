8ecaf7d16dbc8f60108a4171358bdc1d4b61a19c
Kamil Pytlak <51530331+kamilpytlak@users.noreply.github.com>
2022-05-27 21:38:39 +0200

    Corrections to columns positions

--- apps/home.py
+++ apps/home.py
@@ -3,11 +3,10 @@ import streamlit as st
 HOME_LOGO = 'assets/images/bioinf_image.png'
 HOME_INFO = open('assets/info/home_info.md').read()
 
+
 def app():
-    col_1, col_2 = st.columns([1, 3])
-
-    with col_1:
+    col_image, col_text = st.columns([1, 3])
+    with col_image:
         st.image(HOME_LOGO)
-    with col_2:
+    with col_text:
         st.write(HOME_INFO)
-
